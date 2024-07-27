7744eb1b1fc39df7650027d77ef03654ed8aab63
Rahil RiyazAhmed Saiyed <102587430+rahilsaiyed1711@users.noreply.github.com>
2024-07-27 12:52:22 +0530

    Update twentySeven.py

--- twentySeven.py
+++ twentySeven.py
@@ -17,3 +17,33 @@
     }
 
 
+def save_data():
+    block = request.args.get('block')
+    plant = request.args.get('plant')
+    data = request.json
+    print(data)
+    block_df = pd.DataFrame(data)
+
+    filter_plant = df[df['Plantname'] == plant]
+    mytable = filter_plant.Tablename.unique()
+    mytable = listToString(mytable)
+    mytable = remove(mytable)
+
+    selectQuery = "SELECT * FROM agel-svc-winddata-dmz-prod.winddata." + mytable
+    df11 = client.query(selectQuery).to_dataframe()
+
+    df22 = df11[df11['Block'] != block].append(block_df, ignore_index=True)
+    df33 = df22[df22['Plant'] == plant]
+
+    print(df22)
+
+    csv_file_path = "D:\\OneDrive - Adani\\Documents\\" + plant + ".csv"
+    df33.to_csv(csv_file_path, index=False)
+
+    table_id = "agel-svc-winddata-dmz-prod.winddata." + mytable
+    table = bigquery.Table(table_id)
+    job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = "WRITE_TRUNCATE"  # Overwrite table truncate
+    df22 = df22.astype(str)
+    job = client.load_table_from_dataframe(df22, table, job_config=job_config)
+    return jsonify({"message": "Data saved successfully"})
